d21d300e04df856749b68fcd1d1d6e48685d2220
Ozodbek Tursunaliyev <ozodbektursunaliyev11@gmail.com>
2024-12-31 12:24:13 +0500

    add Tailwind CSS configuration and SVG icons; create .gitignore for node_modules

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      poppins: ["Poppins", "system-ui"],
+    },
+  },
+  plugins: [],
+};
